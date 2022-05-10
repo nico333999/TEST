4854e684d5c388f400b051c0cde113ee802b22f7
NICOLASHERENCIA <nicoherencia@gmail.com>
2022-05-10 18:21:57 -0500

    se imlemento el metodo sub

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,9 @@ public class Calculator {
 int add(int a,int b){
 	return a+b;
 }
-int sub(int a,int b){return 0;}
+int sub(int a,int b){
+	return a-b;
+}
 int mul(int a,int b){return 0;}
 int div (int a,int b){return 0;}
 int mod(int a,int b){return 0;}
