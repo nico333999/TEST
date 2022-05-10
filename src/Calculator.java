6bc1bd359a633627f943ec94a7bdbf5540ac0fc8
NICOLASHERENCIA <nicoherencia@gmail.com>
2022-05-10 18:00:29 -0500

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+int add(int a,int b){return 0;}
+int sub(int a,int b){return 0;}
+int mul(int a,int b){return 0;}
+int div (int a,int b){return 0;}
+int mod(int a,int b){return 0;}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
